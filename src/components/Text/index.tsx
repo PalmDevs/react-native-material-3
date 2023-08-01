50a001b4d91beebc8a90b0a00ef4b9168f77a0f7
Palm <palmpasuthorn@gmail.com>
2023-08-02 01:17:17 +0700

    feat(components): add Text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,113 @@
+import { useContext } from 'react'
+import {
+    Animated as AnimatedAPI,
+    Text as NativeText,
+    TextStyle as NativeTextStyle,
+    TextProps,
+} from 'react-native'
+import InternalColorContext from '../../contexts/InternalColorContext'
+import * as TypeScale from '../../constants/TypeScale'
+
+const createScaledNativeTextComponent =
+    (textStyle: NativeTextStyle) => (props: TextProps) => {
+        const colors = useContext(InternalColorContext)
+
+        return (
+            <NativeText
+                {...props}
+                style={[
+                    {
+                        color: colors.onSurface,
+                    },
+                    textStyle,
+                    props.style,
+                ]}
+            >
+                {props.children}
+            </NativeText>
+        )
+    }
+
+const createScaledNativeAnimatedTextComponent =
+    (textStyle: NativeTextStyle) => (props: TextProps) => {
+        const colors = useContext(InternalColorContext)
+
+        return (
+            <AnimatedAPI.Text
+                {...props}
+                style={[
+                    {
+                        color: colors.onSurface,
+                    },
+                    textStyle,
+                    props.style,
+                ]}
+            >
+                {props.children}
+            </AnimatedAPI.Text>
+        )
+    }
+
+export default {
+    Body: {
+        Small: createScaledNativeTextComponent(TypeScale.Body.Small),
+        Medium: createScaledNativeTextComponent(TypeScale.Body.Medium),
+        Large: createScaledNativeTextComponent(TypeScale.Body.Large),
+    },
+    Display: {
+        Small: createScaledNativeTextComponent(TypeScale.Display.Small),
+        Medium: createScaledNativeTextComponent(TypeScale.Display.Medium),
+        Large: createScaledNativeTextComponent(TypeScale.Display.Large),
+    },
+    Headline: {
+        Small: createScaledNativeTextComponent(TypeScale.Headline.Small),
+        Medium: createScaledNativeTextComponent(TypeScale.Headline.Medium),
+        Large: createScaledNativeTextComponent(TypeScale.Headline.Large),
+    },
+    Label: {
+        Small: createScaledNativeTextComponent(TypeScale.Label.Small),
+        Medium: createScaledNativeTextComponent(TypeScale.Label.Medium),
+        Large: createScaledNativeTextComponent(TypeScale.Label.Large),
+    },
+    Title: {
+        Small: createScaledNativeTextComponent(TypeScale.Title.Small),
+        Medium: createScaledNativeTextComponent(TypeScale.Title.Medium),
+        Large: createScaledNativeTextComponent(TypeScale.Title.Large),
+    },
+}
+
+export const Animated = {
+    Body: {
+        Small: createScaledNativeAnimatedTextComponent(TypeScale.Body.Small),
+        Medium: createScaledNativeAnimatedTextComponent(TypeScale.Body.Medium),
+        Large: createScaledNativeAnimatedTextComponent(TypeScale.Body.Large),
+    },
+    Display: {
+        Small: createScaledNativeAnimatedTextComponent(TypeScale.Display.Small),
+        Medium: createScaledNativeAnimatedTextComponent(
+            TypeScale.Display.Medium
+        ),
+        Large: createScaledNativeAnimatedTextComponent(TypeScale.Display.Large),
+    },
+    Headline: {
+        Small: createScaledNativeAnimatedTextComponent(
+            TypeScale.Headline.Small
+        ),
+        Medium: createScaledNativeAnimatedTextComponent(
+            TypeScale.Headline.Medium
+        ),
+        Large: createScaledNativeAnimatedTextComponent(
+            TypeScale.Headline.Large
+        ),
+    },
+    Label: {
+        Small: createScaledNativeAnimatedTextComponent(TypeScale.Label.Small),
+        Medium: createScaledNativeAnimatedTextComponent(TypeScale.Label.Medium),
+        Large: createScaledNativeAnimatedTextComponent(TypeScale.Label.Large),
+    },
+    Title: {
+        Small: createScaledNativeAnimatedTextComponent(TypeScale.Title.Small),
+        Medium: createScaledNativeAnimatedTextComponent(TypeScale.Title.Medium),
+        Large: createScaledNativeAnimatedTextComponent(TypeScale.Title.Large),
+    },
+}
