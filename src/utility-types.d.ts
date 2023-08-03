e3fd934a4d766708ae6b6f22969f866a8291e33b
Palm <palmpasuthorn@gmail.com>
2023-08-03 18:46:53 +0700

    fix(types): do not declare in global scope and remove useless types

--- src/utility-types.d.ts
+++ src/utility-types.d.ts
@@ -1,18 +1,12 @@
 import { ImageStyle, TextStyle, ViewStyle } from 'react-native'
 
-declare type NoReadonlyProperties<T> = {
+export declare type NoReadonlyProperties<T> = {
     -readonly [P in keyof T]: T[P]
 }
 
-declare type AnyStyle = ViewStyle | TextStyle | ImageStyle
+export declare type AnyStyle = ViewStyle | TextStyle | ImageStyle
 
-declare type StylesCreatorReturnType = Record<
+export declare type StylesCreatorReturnType = Record<
     string,
     AnyStyle | StyleSheet.NamedStyles<any>
 >
-
-declare global {
-    interface ObjectConstructor {
-        keys<T>(obj: T): Array<keyof T>
-    }
-}
