473f81b7b34704f8ce29b2042c36ac3b50ef46db
Palm <palmpasuthorn@gmail.com>
2023-09-16 23:28:55 +0700

    fix(components)!: rename conflicting `View` component to `Surface`

--- src/components/Surface.tsx
+++ src/components/Surface.tsx
@@ -1,9 +1,9 @@
-import { View, ViewProps as NativeViewProps } from 'react-native'
+import { View, ViewProps } from 'react-native'
 import { useMaterial3ColorScheme } from '../hooks'
 import { Material3ColorScheme } from '..'
 import { useMemo } from 'react'
 
-export default function Material3View(props: ViewProps) {
+export default function Surface(props: SurfaceProps) {
     const colors = useMaterial3ColorScheme()
     const backgroundColor = useMemo(
         () => props.renderBackgroundColor?.(colors) ?? colors.surface,
@@ -17,6 +17,6 @@ export default function Material3View(props: ViewProps) {
     )
 }
 
-export interface ViewProps extends NativeViewProps {
+export interface SurfaceProps extends ViewProps {
     renderBackgroundColor?: (colors: Material3ColorScheme) => string
 }
